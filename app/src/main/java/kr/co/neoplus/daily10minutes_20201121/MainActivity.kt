9d5ba11612dd4915ba4d6d8f797eb93b24fcea14
kimhaun <wha02068@gmail.com>
2020-11-21 15:37:09 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.neoplus.daily10minutes_20201121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
+    }
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}

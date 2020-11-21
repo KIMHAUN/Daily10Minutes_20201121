ad30ead7e1fd1eb09ecfea56f6d92880f0437cb9
kimhaun <wha02068@gmail.com>
2020-11-21 15:45:16 +0900

    버튼 눌리면 입력한 아이디/비번 추출까지만

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.neoplus.daily10minutes_20201121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,18 @@ class MainActivity : BaseActivity() {
         setupEvents()
     }
     override fun setupEvents() {
+
+        signInBtn.setOnClickListener {
+//            1. 입력된 아이디 / 비번 받아오기
+            val inputEmail = emailEdt.text.toString()
+            val inputPwd = pwdEdt.text.toString()
+
+//            2. 서버에 맞는 회원인지 확인(로그인 API 호출) => 서버 응답 분석=> UI 반영
+
+
+
+
+        }
     }
 
     override fun setValues() {

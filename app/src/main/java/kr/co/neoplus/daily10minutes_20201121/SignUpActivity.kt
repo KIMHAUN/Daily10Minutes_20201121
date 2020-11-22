72698c10787956e71b5cb50febccf598763156d2
kimhaun <wha02068@gmail.com>
2020-11-22 12:17:26 +0900

    회원가입 화면 모양만 잡아서 추가

---
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.neoplus.daily10minutes_20201121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}

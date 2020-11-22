7f3eafd863f12a1e748383d885547d98c7d95c94
kimhaun <wha02068@gmail.com>
2020-11-22 13:40:27 +0900

    회원가입 성공시 => 사용자 닉네임까지 받아와서 토스트로 출력.

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SignUpActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SignUpActivity.kt
@@ -2,6 +2,7 @@ package kr.co.neoplus.daily10minutes_20201121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
 import org.json.JSONObject
@@ -22,12 +23,38 @@ class SignUpActivity : BaseActivity() {
             val inputNickName = nickNameEdt.text.toString()
 
 //            2. ServerUtil활용 => 회원가입 API 호출
-            ServerUtil.putRequestSignUp(mContext, inputId, inputPwd, inputNickName, object: ServerUtil.JsonResponseHandler{
-                override fun onResponse(json: JSONObject) {
+            ServerUtil.putRequestSignUp(
+                mContext,
+                inputId,
+                inputPwd,
+                inputNickName,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
 //            3. 돌아와서 어떡할지 ?
+                        val code = json.getInt("code")
+                        if (code == 200) {
+//                        가입 성공 : 가입한 사람의 닉네임을 가지고 환영 토스트
+//                        nick님 환영합니다! 등
+//                        로그인 화면으로 복귀
 
-                }
-            })
+                            val dataObj = json.getJSONObject("data")
+                            val userObj = dataObj.getJSONObject("user")
+                            val nickName = userObj.getString("nick_name")
+
+                            runOnUiThread {
+                                Toast.makeText(mContext, "${nickName}님, 환영합니다!", Toast.LENGTH_SHORT).show()
+                                finish()
+                            }
+
+                        } else {
+//                        가입 실패 : message 적힌 가입 실패 사유 받아서 출력
+                            val message = json.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                })
         }
     }
 

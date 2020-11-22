8855fb62f6a1dee224258116310b74e99a57e9e8
kimhaun <wha02068@gmail.com>
2020-11-22 11:49:18 +0900

    서버 응답 json변수에서 code값(Int) 추출   =>runOnUiThread{}안에 토스트를 띄워야 앱이 안죽음.

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.neoplus.daily10minutes_20201121
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
 import org.json.JSONObject
@@ -22,13 +23,33 @@ class MainActivity : BaseActivity() {
             val inputPwd = pwdEdt.text.toString()
 
 //            2. 서버에 맞는 회원인지 확인(로그인 API 호출) => 서버 응답 분석=> UI 반영
-            ServerUtil.postRequestLogin(mContext, inputEmail, inputPwd, object: ServerUtil.JsonResponseHandler{
+            ServerUtil.postRequestLogin(mContext, inputEmail, inputPwd, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 //                    로그인 API를 다녀오면 실행할 내용
 //                    다녀 온다 : 서버가 Response를 준다.
 //                    그 응답 내용 : json 변수에 담겨 있다.
 
                     Log.d("화면 : 서버 다녀옴", json.toString())
+
+                    //서버가 알려주는code값 추출해서 LOG로 출력
+                    val codeNum = json.getInt("code")
+
+                    Log.d("코드값", codeNum.toString())
+
+//                    받아낸 코드값으로 로그인 성공/실패를 토스트로 출력
+                    if (codeNum == 200) {
+//                        login success
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }else{
+//                        login fail
+                        runOnUiThread{
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
                 }
             })
         }

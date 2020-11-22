0da978c495d833955519d772593706efa665bec1
kimhaun <wha02068@gmail.com>
2020-11-22 16:06:06 +0900

    app 시작 화면 SplashActivity.kt로 변경

---
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SplashActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.neoplus.daily10minutes_20201121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

09c651cee1c0d1d471cd8be953141de508b32c06
kimhaun <wha02068@gmail.com>
2020-11-22 10:31:54 +0900

    로그인 함수 초안 구현 => 요청까지 진행

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/utils/ServerUtil.kt
@@ -1,6 +1,8 @@
 package kr.co.neoplus.daily10minutes_20201121.utils
 
+import okhttp3.FormBody
 import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class ServerUtil {
 
@@ -11,7 +13,7 @@ class ServerUtil {
         val BASE_URL = "http://15.164.153.174"
 //      로그인 기능 수행 함수.
 
-        fun postRequestLogin(email:String, password:String){
+        fun postRequestLogin(id:String, pw:String){
 //            클라이언트의 역할을 수행해주는 변수(라이브러리 활용)
             val client = OkHttpClient()
 
@@ -19,6 +21,37 @@ class ServerUtil {
 //            BASE_URL/user
 
             val urlString = "${BASE_URL}/user"
+
+//            POST 메소드 - 보통 formBody(OkHttp 라이브러리가 제공) 에 데이터 첨부
+//            Builder의 마무리 .build() 등
+            val formData = FormBody.Builder()
+                    .add("email", id)
+                    .add("password", pw)
+                    .build()
+
+//            myIntent 변수를 만들듯이 API Request 정보를 담는 변수도 만들어야 함.
+//            요청에 대한 모든 정보 종합하는 역할.
+//            1. 어디로? url
+//            2. 어떤 방식? method(POST)
+//            3. 갈 때 어떤 데이터? formData에 담아둠.
+//            + header 데이터 요청이 있다면 여ㅣ서 추가.
+
+            val request = Request.Builder()
+                    .url(urlString)
+                    .post(formData)
+//                    .header("이름표", "실제 값") - 서버가 헤더 데이터를 요구하면 주석 해제.
+                    .build()
+
+//            startActivity처럼 실제로 Request를 실행시키는 함수.
+//            클라이언트로 동작하는 행위(Request 호출)
+//            OktHttp 라이브러리 => client 변수 활용
+            client.newCall(request)
+
+
+
+
+
+
 
 
 

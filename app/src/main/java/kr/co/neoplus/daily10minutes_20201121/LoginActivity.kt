e6612cce2b1da0c54a6ca3221c656d9c2e1dbe6b
kimhaun <wha02068@gmail.com>
2020-11-22 15:55:50 +0900

    서버가 주는 토큰 값 추출 => ContextUtil 이용해서 SharedPreferences에 저장

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/LoginActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import kr.co.neoplus.daily10minutes_20201121.utils.ContextUtil
 
 import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
 import org.json.JSONObject
@@ -49,9 +50,15 @@ class LoginActivity : BaseActivity() {
                         runOnUiThread {
                             Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
 
+//                            서버가 알려주는 토큰 추출 json{} => data{} => token String 추출
+                            val dataObj = json.getJSONObject("data")
+                            val token = dataObj.getString("token")
+
 //                            받은 토큰 값을 저장. (다른 화면에서도 활용) 나의 토큰 : eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6ODAsImVtYWlsIjoid2hhMDIwNjhAZ21haWwuY29tIiwicGFzc3dvcmQiOiIwYWQ4NmMwZDA1NDA0YzQwZGY2MGU4MzlhYTRjMjdlMiJ9.yRk9TXyHjSJdpXyD2YooeyBTTt0UZKZSALzWtpUnitYW4qz2enTb3kJJrhvM3KExrxtZUv_RmbZX3p_nov_HvQ
+                            ContextUtil.setLoginUserToken(mContext, token)
 
 //                            메인화면으로 진입 + 로그인 화면 종료
+
                         }
 
                     }else{

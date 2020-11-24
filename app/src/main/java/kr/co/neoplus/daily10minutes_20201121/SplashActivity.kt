ada067a54a51184b0e11d3858d5523205af9416f
KIMHAUN <wha02068@gmailcom>
2020-11-24 13:11:39 +0900

    스플래쉬 적용

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SplashActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/SplashActivity.kt
@@ -22,26 +22,26 @@ class SplashActivity : BaseActivity() {
 //        토큰 검사는 2.5초 후에 실행되도록 지연 처리
         val myHandler = Handler(Looper.getMainLooper())
         myHandler.postDelayed({
+//        토큰 값이 어떻게 저장되어있는지 체크.
+//        "" : 아직 저장된 토큰이 없다. 로그인이 필요한 상태다. => 로그인 화면 이동
+//        그 외의 값 : 저장된 토큰이 있다. 자동 로그인 처리. => 메인 화면으로 이동
 
+            if(ContextUtil.getLoginUserToken(mContext) == ""){
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+
+            }else{
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+//        이동 후에는 로딩 화면 종료
+            finish()
 
         },2500)
 
 
 
-//        토큰 값이 어떻게 저장되어있는지 체크.
-//        "" : 아직 저장된 토큰이 없다. 로그인이 필요한 상태다. => 로그인 화면 이동
-//        그 외의 값 : 저장된 토큰이 있다. 자동 로그인 처리. => 메인 화면으로 이동
 
-        if(ContextUtil.getLoginUserToken(mContext) == ""){
-            val myIntent = Intent(mContext, LoginActivity::class.java)
-            startActivity(myIntent)
-
-        }else{
-            val myIntent = Intent(mContext, MainActivity::class.java)
-            startActivity(myIntent)
-        }
-//        이동 후에는 로딩 화면 종료
-        finish()
 
     }
 

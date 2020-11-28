62a8c8263f5f331d9f79c9b7d701aaeb89da7728
kimhaun <wha02068@gmail.com>
2020-11-28 14:49:33 +0900

    참여중인 사용자 명단

---
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.neoplus.daily10minutes_20201121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProjectUserListActivity : BaseActivity() {
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_project_user_list)
+    }
+}

a442984d2ff330479e567e0e7087eb0ca9bb51ff
kimhaun <wha02068@gmail.com>
2020-11-28 15:37:16 +0900

    사용자 명단 배열까지 분석 => 이 내부를 반복적으로 파싱할 준비작업

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kr.co.neoplus.daily10minutes_20201121.datas.Project
 import kr.co.neoplus.daily10minutes_20201121.datas.User
 import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewProjectUserListActivity : BaseActivity() {
 
@@ -32,6 +33,20 @@ class ViewProjectUserListActivity : BaseActivity() {
 
     fun getUserListFromServer(){
 //        서버에 접근해서 사용자 목록 받아오는 작업. => 내용 분석 + mUserList에 담아주기
-        //ServerUtil.getProjectUserList()
+        ServerUtil.getProjectUserList(mContext, mProject.id, object:ServerUtil.JsonResponseHandler{
+//            갔다와서 뭐할건가요??
+            override fun onResponse(json: JSONObject) {
+                val dataObj= json.getJSONObject("data")
+                val projectObj = dataObj.getJSONObject("project")
+
+//                 project{} 안에 있는 onging_users[]를 가져다가 분석하자.
+                val onGoingUsersArr = projectObj.getJSONArray("ongoing_users")
+
+//              for문 이용 0 ~ 들어 있는 사용자 수 직전까지 돌자.
+                for(i in 0 until onGoingUsersArr.length()){
+
+                }
+            }
+        })
     }
 }

a4eb2593ffe4b9fbc4b1dce8382dec8b1d99be19
kimhaun <wha02068@gmail.com>
2020-11-28 10:32:12 +0900

    프로젝트 표현용 데이터 클래스 생성   - API테스트를 보고 그 내용을 담기 위해 설계

---
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/Project.kt
@@ -0,0 +1,13 @@
+package kr.co.neoplus.daily10minutes_20201121.datas
+
+class Project {
+
+    var id = 0 //id에는 정수가 들어올 거라고 명시
+    var title = "" //title 에는 String이 들어옴.
+    var imageURL = ""
+    var description = ""
+    var proofMethod = ""
+    var completeDays = 0 // 완주 횟수
+    var onGoingUsersCount = 0 //진행중인 유저 수
+
+}

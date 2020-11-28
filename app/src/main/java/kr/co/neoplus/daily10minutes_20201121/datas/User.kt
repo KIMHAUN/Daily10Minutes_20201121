f3faf8ad175e537bc8bacd5e6d56843065a5f55a
kimhaun <wha02068@gmail.com>
2020-11-28 16:04:20 +0900

    사용자 정보 파싱시 - 프로필 이미지 목록도 파싱해서 사용자 프사 목록으로 등록.....

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/User.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/User.kt
@@ -12,6 +12,10 @@ class User : Serializable {
 //    1. 가입 일시(created_at) 서버는 String으로 알려줌. 앱에서는 Calendar 형태로 변환해서 사용.
 //    2. 프로필 사진들 : 한 명의 유저가 여러 장의 사진URL (String)을 갖고 있음을 어떻게 표현하는가?
 
+//    한 명의 사용자 : 프사 URL 목록을 갖는다고 명시. (ArrayList를 갖는다.)
+    val profileImgList = ArrayList<String>()
+
+
 //    JSON넣으면 User로 변환해주는 기능 제작
     companion object{
         fun getUserFromJSON(json : JSONObject) : User{
@@ -20,6 +24,14 @@ class User : Serializable {
             u.id = json.getInt("id")
             u.email = json.getString("email")
             u.nickName = json.getString("nick_name")
+
+//            프사 목록 파싱 u가 가진 profileImgList에 add해주자.
+            val profileImagesArr = json.getJSONArray("profile_images")
+            for(i in 0 until profileImagesArr.length()){
+                val pf = profileImagesArr.getJSONObject(i)
+                val imgURL = pf.getString("img_url")
+                u.profileImgList.add(imgURL)
+            }
 
             return u
         }

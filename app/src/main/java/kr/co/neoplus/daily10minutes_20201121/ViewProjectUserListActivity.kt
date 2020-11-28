4a92781cf49d65e34878f6c3f5db61faa681f6e9
kimhaun <wha02068@gmail.com>
2020-11-28 14:53:41 +0900

    프로젝트 명단 확인시 어떤 프로젝트인지 넘겨주기까지

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -7,16 +7,19 @@ import kr.co.neoplus.daily10minutes_20201121.datas.Project
 class ViewProjectUserListActivity : BaseActivity() {
 
     lateinit var mProject : Project
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_project_user_list)
+        setupEvents()
+        setValues()
+    }
+
     override fun setupEvents() {
 
     }
 
     override fun setValues() {
         mProject = intent.getSerializableExtra("project") as Project
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_project_user_list)
     }
 }

e24d589baadba6dc705017e8f69a3bb443a07500
kimhaun <wha02068@gmail.com>
2020-11-28 15:41:18 +0900

    알맞는 JSON을 User로 변환하는 기능

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/User.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/User.kt
@@ -1,5 +1,6 @@
 package kr.co.neoplus.daily10minutes_20201121.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class User : Serializable {
@@ -11,5 +12,16 @@ class User : Serializable {
 //    1. 가입 일시(created_at) 서버는 String으로 알려줌. 앱에서는 Calendar 형태로 변환해서 사용.
 //    2. 프로필 사진들 : 한 명의 유저가 여러 장의 사진URL (String)을 갖고 있음을 어떻게 표현하는가?
 
+//    JSON넣으면 User로 변환해주는 기능 제작
+    companion object{
+        fun getUserFromJSON(json : JSONObject) : User{
+            val u = User()
+//            u의 변수들을 json이 가진 값을 활용해서 변경
+            u.id = json.getInt("id")
+            u.email = json.getString("email")
+            u.nickName = json.getString("nick_name")
 
+            return u
+        }
+    }
 }

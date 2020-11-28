d98240e3c8c813c199554e32ad3072c17777b7d6
kimhaun <wha02068@gmail.com>
2020-11-28 15:21:37 +0900

    사용자 명단 가져오는 API호출 기능 추가

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -3,10 +3,14 @@ package kr.co.neoplus.daily10minutes_20201121
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.co.neoplus.daily10minutes_20201121.datas.Project
+import kr.co.neoplus.daily10minutes_20201121.datas.User
+import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
 
 class ViewProjectUserListActivity : BaseActivity() {
 
     lateinit var mProject : Project
+
+    val mUserList = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,13 @@ class ViewProjectUserListActivity : BaseActivity() {
 
     override fun setValues() {
         mProject = intent.getSerializableExtra("project") as Project
+
+//        서버에서 사용자 목록 가져오는 함수 별도 실행.
+        getUserListFromServer()
+    }
+
+    fun getUserListFromServer(){
+//        서버에 접근해서 사용자 목록 받아오는 작업. => 내용 분석 + mUserList에 담아주기
+        //ServerUtil.getProjectUserList()
     }
 }
--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/utils/ServerUtil.kt
@@ -324,5 +324,39 @@ class ServerUtil {
             })
         }
 
+//      프로젝트의 참여자 명단 가져오기(needUserList는 true로 박음.)
+        fun getProjectUserList(context: Context, projectId: Int, handler:JsonResponseHandler?){
+            val client = OkHttpClient()
+
+            //          복잡한 주소를 가공해 나갈 때 필요한 기본 재료(URL 가공기)
+            val urlBuilder = "${BASE_URL}/project/${projectId}".toHttpUrlOrNull()!!.newBuilder()
+            //          url가공기를 이용해서 필요한 파라미터들을 쉽게 첨부 ?email=wha02079@naver.com
+            urlBuilder.addEncodedQueryParameter("need_user_list", "true")
+
+            // 가공이 끝난 URL을 urlString으로 변경
+            val urlString = urlBuilder.build().toString()
+            //Log.d("완성된 URL", urlString)
+            //          요청 정보를 종합하는 Request 생성
+            val request = Request.Builder()
+                .url(urlString)
+                .get() //이미 urlString에 파라미터 다들어있어서 넣을 필요 없음.
+                .header("X-Http-Token", ContextUtil.getLoginUserToken(context))
+                .build()
+
+            //      회원가입꺼 client 복붙
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Toast.makeText(context, "서버에 문제가 있씁니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버 응답 본문", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+                }
+            })
+        }
+
     }
 }

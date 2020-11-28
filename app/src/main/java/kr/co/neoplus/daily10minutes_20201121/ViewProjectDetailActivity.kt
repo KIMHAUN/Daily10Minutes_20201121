d2056b2653008fe7d72be1652b30af30d7f4283b
kimhaun <wha02068@gmail.com>
2020-11-28 13:54:47 +0900

    참여 신청 API 실제 사용   - 같은 기능을 하기 위한 똑같은 코드를 여러 번 적는건 매우 비효율적인 코드 => 별도 함수/ 클래스 기능으로 만들어두고, 가져다 쓰게 하자.(모듈화)

--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectDetailActivity.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/ViewProjectDetailActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_view_project_detail.*
 import kr.co.neoplus.daily10minutes_20201121.datas.Project
 import kr.co.neoplus.daily10minutes_20201121.utils.ContextUtil
 import kr.co.neoplus.daily10minutes_20201121.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewProjectDetailActivity : BaseActivity() {
 
@@ -23,28 +24,44 @@ class ViewProjectDetailActivity : BaseActivity() {
 
         //나도 도전하기!
         applyBtn.setOnClickListener {
+            ServerUtil.postRequestApplyProject(mContext, mProject.id, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    val dataObj = json.getJSONObject("data")
+                    val projectObj = dataObj.getJSONObject("project")
 
+//                    참여신청하면 최신상태로 변경된 프로젝트 정보를 서버가 다시 알려줌.
+//                    그 모든 정보가 담겨있는 새 프로잭트 json : projectObj
 
+                    val newProject = Project.getProjectFromJSON(projectObj)
+
+//                    화면에는 mProject가 뿌려짐
+                    mProject = newProject
+
+//                    새 프로젝트 데이터를 다시 뿌려주자. UI 변경
+                    runOnUiThread {
+                        setProjectDataToUI()
+                    }
+                }
+            })
         }
-
-
     }
 
     override fun setValues() {
         mProject = intent.getSerializableExtra("project") as Project
+        setProjectDataToUI()
 
-//        mProject 활용 => 실제 데이터 화면에 뿌려주기.
+    }
+
+//    mProject로 화면에 뿌려주는 함수
+    fun setProjectDataToUI(){
+    //        mProject 활용 => 실제 데이터 화면에 뿌려주기.
 
         projectTitleTxt.text = mProject.title
         Glide.with(mContext).load(mProject.imageURL).into(projectImg)
         projectDescription.text = mProject.description
         projectProofMethodTxt.text = mProject.proofMethod
 
-//        서버가 주는 데이터 : 5 숫자 ->String 가공
+    //        서버가 주는 데이터 : 5 숫자 ->String 가공
         onGoingUsersCountTxt.text = "지금 ${mProject.onGoingUsersCount}명 참여중!"
-
-
-
-
     }
 }
--- app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/Project.kt
+++ app/src/main/java/kr/co/neoplus/daily10minutes_20201121/datas/Project.kt
@@ -1,5 +1,6 @@
 package kr.co.neoplus.daily10minutes_20201121.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class Project : Serializable {
@@ -11,5 +12,28 @@ class Project : Serializable {
     var proofMethod = ""
     var completeDays = 0 // 완주 횟수
     var onGoingUsersCount = 0 //진행중인 유저 수
+    
+    companion object{
+        
+//        적당한 모양의 JSON {}를 넣으면 => Project 객체로 변환해 주는 기능.
+//        액티비티 등 다른 곳에서는 이 기능을 가져다 사용만 하자.
+        
+        fun getProjectFromJSON(json: JSONObject) : Project{
+//            기본 데이터만 갖고 있는 Project() 생성
+            val project = Project()
+            
+//            재료로 들어온 JSON내용을 파싱해서 project 변수의데이터 교체
+            project.id = json.getInt("id")
+            project.title = json.getString("title")
+            project.imageURL = json.getString("img_url")
+            project.description = json.getString("description")
+            project.proofMethod = json.getString("proof_method")
+            project.completeDays = json.getInt("complete_days")
+            project.onGoingUsersCount = json.getInt("ongoing_users_count")
+            
+//            완성된 project 변수 리턴
+            return project 
+        }        
+    }
 
 }
